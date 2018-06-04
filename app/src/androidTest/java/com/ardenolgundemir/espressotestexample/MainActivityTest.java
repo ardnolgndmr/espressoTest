bfeaf6178e277bb18555dfdee456ddacad39d579
ardnolgndmr <aolgundemir@gmail.com>
2018-06-04 17:43:49 +0300

    background api listener TESTING

--- app/src/androidTest/java/com/ardenolgundemir/espressotestexample/MainActivityTest.java
+++ app/src/androidTest/java/com/ardenolgundemir/espressotestexample/MainActivityTest.java
@@ -1,31 +1,32 @@
 package com.ardenolgundemir.espressotestexample;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.InstrumentationTestCase;
 import android.util.Log;
 
 import com.ardenolgundemir.espressotestexample.api.WebServices;
+import com.ardenolgundemir.espressotestexample.api.model.UserModel;
 import com.ardenolgundemir.espressotestexample.controller.activity.MainActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.action.ViewActions.click;
-import static org.hamcrest.Matchers.not;
 
 /**
  * Created by ardenolgundemir on 29.05.2018.
@@ -35,34 +36,49 @@ public class MainActivityTest extends InstrumentationTestCase{
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
-
-    WebServices webServices;
-    @Before
-    public void setUp(){
-
-        Log.i("MainActivityTest", getClass().getName().toLowerCase());
-        webServices = mainActivityActivityTestRule.getActivity().getService();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-
-    }
-
-    //Visible Test
     @Test
-    public void onHelloTestVisible() throws Exception{
+    public void useAppContext() throws Exception {
+        CountingIdlingResource mainActivityIdlingResource = mainActivityActivityTestRule.getActivity().getEspressoIdlingResourceForMainActivity();
+
+        // registering MainActivity's idling resource for enabling Espresso sync with MainActivity's background threads
+        Espresso.registerIdlingResources(mainActivityIdlingResource);
+
         onView(withText("Hello World!")).check(matches(isDisplayed()));
-        if (webServices != null){
-
-        }
-    }
-
-    //Start Activity Test
-    @Test
-    public void onStartActivity() throws Exception{
-        Intents.init();
+        onView(withId(R.id.tvUserName)).check(matches(isDisplayed()));
         onView(withId(R.id.btn)).perform(click());
-        intended(hasComponent(NewActivity.class.getName()));
-        onView(withText("Hello Espresso!")).check(matches(isDisplayed()));
-        Intents.release();
+        onView(withId(R.id.tvUserName)).check(matches(withText("ardnolgndmr")));
     }
+
+//
+//    //Visible Test
+//    @Test
+//    public void onHelloTestVisible() throws Exception{
+//        onView(withText("Hello World!")).check(matches(isDisplayed()));
+//        onView(withId(R.id.tvUserName)).check(matches(isDisplayed()));
+//        onView(withId(R.id.btn)).perform(click());
+//        if (webServices != null){
+//
+//            webServices.getUser("ardnolgndmr")
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<UserModel>() {
+//            @Override
+//            public void call(UserModel userModel) {
+//                if (userModel != null){
+//                    onView(withId(R.id.tvUserName)).check(matches(withText("ardnolgndmr")));
+//                }
+//            }
+//        }, throwable -> {});
+//        }
+//    }
+//
+////    //Start Activity Test
+////    @Test
+////    public void onStartActivity() throws Exception{
+////        Intents.init();
+////        onView(withId(R.id.btn)).perform(click());
+////        intended(hasComponent(NewActivity.class.getName()));
+////        onView(withText("Hello Espresso!")).check(matches(isDisplayed()));
+////        Intents.release();
+////    }
 
 }
--- app/src/main/java/com/ardenolgundemir/espressotestexample/controller/activity/MainActivity.java
+++ app/src/main/java/com/ardenolgundemir/espressotestexample/controller/activity/MainActivity.java
@@ -2,12 +2,14 @@ package com.ardenolgundemir.espressotestexample.controller.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.test.espresso.idling.CountingIdlingResource;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.ardenolgundemir.espressotestexample.NewActivity;
 import com.ardenolgundemir.espressotestexample.R;
+import com.ardenolgundemir.espressotestexample.api.WebServices;
 import com.ardenolgundemir.espressotestexample.api.model.UserModel;
 import com.ardenolgundemir.espressotestexample.controller.BaseActivity;
 
@@ -22,29 +24,43 @@ import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
-
-    @Override
+    CountingIdlingResource espressoTestIdlingResource = new CountingIdlingResource("Network_Call");
+    String username = "";
+    TextView tvUserName;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvUserName = (TextView)findViewById(R.id.tvUserName);
+
+
         ((Button)findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TextView)findViewById(R.id.tvTitle)).setVisibility(View.GONE);
-                startActivity(new Intent(MainActivity.this,NewActivity.class));
+                //startActivity(new Intent(MainActivity.this,NewActivity.class));
+                espressoTestIdlingResource.increment();
+                getService().getUser("ardnolgndmr")
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<UserModel>() {
+                    @Override
+                    public void call(UserModel userModel) {
+                        if (userModel != null){
+                            username = userModel.getLogin();
+                            tvUserName.setText(userModel.getLogin());
+                        }
+                        espressoTestIdlingResource.decrement();
+
+                    }
+                }, throwable -> {   espressoTestIdlingResource.decrement();});
             }
         });
 
-        getService().getUser("ardnolgndmr")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<UserModel>() {
-            @Override
-            public void call(UserModel userModel) {
-                    if (userModel != null){
-
-                    }
-            }
-        }, throwable -> {});
+    }
+    public String getUsername(){
+         return username;
+    }
+    public CountingIdlingResource getEspressoIdlingResourceForMainActivity() {
+        return espressoTestIdlingResource;
     }
 }
 
